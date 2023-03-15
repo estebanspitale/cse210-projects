a05a12a14d61bc572e0c12099a80a27a4bc215b6
estebanspitale <esteban_spitalehotmail.com>
2023-03-14 22:56:17 -0300

    Finished learning04 activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
-    }
+        Assignment a = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a.GetSummary());
+    
+        MathAssignment b = new MathAssignment ("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(b.GetSummary());
+        Console.WriteLine(b.GetHomeworkList());
+
+        WritingAssignment c = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(c.GetSummary());
+        Console.WriteLine(c.GetWritingInformation());
+    } 
 }
